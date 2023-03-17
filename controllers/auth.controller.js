ec6f53cd85d631d7145cf3c5664691b5633cab25
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-17 17:03:47 +0545

    AuthController - login, logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -52,3 +52,69 @@ export const signUp = asyncHandler(async (req, res) => {
     })
 
 })
+
+
+
+/*************************************************************************
+    @LOGIN
+    @route http://localhost:4000/api/auth/login
+    @description User login controller for login the user
+    @parameters  email, password
+    @return User Object
+************************************************************************/
+
+export const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new CustomError("Please fill all fields", 400)
+    }
+
+    const user = User.findOne({ email }).select("+password") // we have made password select false in user.schema now we are making it true
+
+    if (!user) {
+        throw new CustomError("Invalid credentials", 400)
+
+    }
+
+    // compare the password
+
+    const isPasswordMatched = await user.comparePassword(password)
+
+    // if password match send the token
+    if (isPasswordMatched) {
+        const token = user.getJwtToken()
+        user.password = undefined
+        res.cookie("token", token, cookieOptions) //  token as a cookie // name of the cookie is token
+        return res.status(200).json({
+            success: true,
+            token,
+            user,
+        })
+    }
+    // if password doesnt match
+    throw new CustomError("Invalid credentials - pass", 400)
+
+})
+
+
+
+/*************************************************************************
+    @LOGOUT
+    @route http://localhost:4000/api/auth/logout
+    @description User logout controller by clearing user cookies
+    @parameters  
+    @return success message
+************************************************************************/
+
+export const logout = asyncHandler(async (_req, res) => { // we are not using req so we use _req _ means not using (not compulsory)
+    // res.clearCookie()
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+    res.status(200).json({
+        success: true,
+        message: "Logged Out"
+    })
+})
