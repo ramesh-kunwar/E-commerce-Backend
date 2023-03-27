0aed62dd36620632a64cd82675c3f0372c867b7e
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2023-03-27 20:42:24 +0545

    Created an aws image upload service

--- config/index.js
+++ config/index.js
@@ -11,7 +11,13 @@ const config = {
     SMPT_MAIL_PORT: process.env.SMPT_MAIL_PORT,
     SMPT_MAIL_USERNAME: process.env.SMPT_MAIL_USERNAME,
     SMPT_MAIL_PASSWORD: process.env.SMPT_MAIL_PASSWORD,
-    SMPT_MAIL_EMAIL: process.env.SMPT_MAIL_EMAIL
+    SMPT_MAIL_EMAIL: process.env.SMPT_MAIL_EMAIL,
+
+    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+    S3_REGION: process.env.S3_REGION
+
 }
 
 export default config
